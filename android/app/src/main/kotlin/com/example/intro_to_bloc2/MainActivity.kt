5c522904be41b37c2054e15e22a8bf28f149f288
reemy <reemormrom98@gmail.com>
2025-05-16 15:53:07 -0400

    Empty the text field after entering letters or numbers

---
+++ android/app/src/main/kotlin/com/example/intro_to_bloc2/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.intro_to_bloc2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/bloc/counter_manager.dart
@@ -0,0 +1,45 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:intro_to_bloc2/bloc/counter_event.dart';
+import 'package:intro_to_bloc2/bloc/counter_state.dart';
+
+class CounterManager extends Bloc<CounterEvent, CounterState> {
+  int counter = 0;
+  CounterManager() : super(initialState()) {
+    on<Increament>(
+      (event, emit) {
+        counter++;
+        print("object");
+        emit(TheNumberHasBeenChange(counter: counter));
+      },
+    );
+
+    on<Decreament>(
+      (event, emit) {
+        counter--;
+        print("new obejec");
+        emit(TheNumberHasBeenChange(counter: counter));
+      },
+    );
+
+    on<Restore>(
+      (event, emit) {
+        counter = 0;
+        emit(RestoreToZero());
+      },
+    );
+
+    on<SetTheNumberFromTextField>(
+      (event, emit) {
+        // TODO:
+        int? temp = int.tryParse(event.theNumber);
+        if (temp == null) {
+          counter = 0;
+          emit(UserInputAlphabetToCounter(counter: counter));
+        } else {
+          counter = temp;
+          emit(TheNumberHasBeenChange(counter: counter));
+        }
+      },
+    );
+  }
+}
